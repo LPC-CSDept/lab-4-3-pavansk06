75da652b86f0c02d5a59a094be95b87783b0b914
pavansk06 <pxkosuru@zonemail.clpccd.edu>
2024-07-01 13:39:08 -0700

    commit 1

--- main.py
+++ main.py
@@ -4,7 +4,15 @@ def main():
     Code Your Program here
     ########################################
     """
+    
+    
     total = 0
+    while i < 5:
+       num = int(input('Enter your input: '))
+       total += num
+       i += 1
+    print(total)
+
 
     ########################################
     # Do not delete the return statement
